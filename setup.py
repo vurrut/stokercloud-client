3a7612770efccd6b8eace36843add24f0f5c09a7
Kristian Øllegaard <kristian@kristian.io>
2022-01-06 08:04:35 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="stokercloud-client",
-    version="0.0.4",
+    version="0.0.5",
     author="Kristian Øllegaard",
     author_email="kristian@kristian.io",
     description="Python stokercloud client",
